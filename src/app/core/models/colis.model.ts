b5dad61c3012ba96d8927738cc424f7444d58540
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2026-01-09 11:28:33 +0100

    feat: add Colis model interface

---
+++ src/app/core/models/colis.model.ts
@@ -0,0 +1,38 @@
+import {Client} from './client.model';
+import {Destinataire} from './destinataire.model';
+import {Livreur} from './livreur.model';
+
+export enum StatutColis {
+  EN_ATTENTE = 'EN_ATTENTE',
+  EN_TRANSIT = 'EN_TRANSIT',
+  LIVRE = 'LIVRE',
+  ANNULE = 'ANNULE',
+  RETOURNE = 'RETOURNE'
+}
+
+export enum PrioriteColis {
+  BASSE = 'BASSE',
+  NORMALE = 'NORMALE',
+  HAUTE = 'HAUTE',
+  URGENTE = 'URGENTE'
+}
+
+export interface Colis {
+  id?: string;
+  description: string;
+  poids: number;
+  priorite: PrioriteColis;
+  statut?: StatutColis;
+  clientExpediteurId: string;
+  clientExpediteur?: Client;
+  destinataireId: string;
+  destinataire?: Destinataire;
+  livreurId?: string;
+  livreur?: Livreur;
+  villeDestination: string;
+  dateLimiteLivraison?: Date;
+  dateCreation?: Date;
+  dateModification?: Date;
+  dateLivraison?: Date;
+  commentaire?: string;
+}
