739caa2ca918de48438074ddbfc03f70a84c379c
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2026-01-09 11:32:16 +0100

    feat: add pagination models for API responses

---
+++ src/app/core/models/pagination.model.ts
@@ -0,0 +1,16 @@
+export interface PageResponse<T> {
+  content: T[];
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  number: number;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
+
+export interface PageRequest {
+  page: number;
+  size: number;
+  sort?: string;
+}
