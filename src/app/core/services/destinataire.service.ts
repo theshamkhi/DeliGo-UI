363bef48e1db3ec06212d1c0e3de0dadc6d4ead6
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2026-01-21 15:19:23 +0100

    feat(services): implement DestinataireService with CRUD

--- src/app/core/services/destinataire.service.ts
+++ src/app/core/services/destinataire.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap, catchError, throwError } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Destinataire } from '../models/destinataire.model';
 import { PageResponse, PageRequest } from '../models/pagination.model';
