fda24bae59769aaa6b4ab3ace34dbf80cc977400
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2026-01-21 15:15:56 +0100

    config: setup app config with HTTP interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([
+        loadingInterceptor,
+        authInterceptor,
+        errorInterceptor
+      ])
+    ),
+    provideAnimationsAsync()
   ]
 };
