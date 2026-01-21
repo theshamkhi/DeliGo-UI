ee1a176d346ca9a6f520a46e537e08b5a52e3418
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2026-01-21 15:22:47 +0100

    feat(interceptors): add loading interceptor for API calls

---
+++ src/app/core/interceptors/loading.interceptor.ts
@@ -0,0 +1,24 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { finalize } from 'rxjs';
+import { LoadingService } from '../services/loading.service';
+
+export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
+  const loadingService = inject(LoadingService);
+
+  // Don't show loading for certain endpoints
+  const skipLoading = req.url.includes('/auth/refresh') ||
+    req.url.includes('/profile');
+
+  if (!skipLoading) {
+    loadingService.show();
+  }
+
+  return next(req).pipe(
+    finalize(() => {
+      if (!skipLoading) {
+        loadingService.hide();
+      }
+    })
+  );
+};
